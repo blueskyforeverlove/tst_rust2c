94487f661a490e51c98f9bd853358eecd5c1f01c
blueskyforeverlove <bluesky@bluepat.cn>
2021-08-17 06:27:33 +0800

    Update main.c

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,7 +1,19 @@
+
+//结构体前置声明;
+struct RustStruct;
+
+//函数声明;
+void RustStruct_show(struct RustStruct* rs);
+struct RustStruct* RustStruct_create();
+void RustStruct_destroy(struct RustStruct* rs);
+
 int main()
 {
-    void* rs = RustStruct_create();
+    //创建对象;
+    struct RustStruct * rs = RustStruct_create();
+    //方法调用;
     RustStruct_show(rs);
+    //销毁对象;
     RustStruct_destroy(rs);
     return 0;
 }
